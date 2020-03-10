2ad848154a47b2cf8a67ef1b63c8949488d4523a
Muhammad Sadiq <msadiqrajani@gmail.com>
2020-03-10 22:49:52 +0500

    Updates

---
+++ precache-manifest.2ed23bf30e70eee5eb6469c3cebdea98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7e766197213dc2d0bc2ebaf3fc5a3ed",
+    "url": "/pixabaysearch/index.html"
+  },
+  {
+    "revision": "181ab18db28e24aa2d5a",
+    "url": "/pixabaysearch/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "32d3e792bbadcf5e8156",
+    "url": "/pixabaysearch/static/js/2.8c946f2e.chunk.js"
+  },
+  {
+    "revision": "6d1ae0a7201d2aa71f08b67ad045a152",
+    "url": "/pixabaysearch/static/js/2.8c946f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "181ab18db28e24aa2d5a",
+    "url": "/pixabaysearch/static/js/main.6a8af1c4.chunk.js"
+  },
+  {
+    "revision": "0e261a501d612fb1903a",
+    "url": "/pixabaysearch/static/js/runtime-main.ab29975a.js"
+  }
+]);
